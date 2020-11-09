061c587dc1455421cd6d26811e6cdffd1d949d84
zyxyz1 <316140666@qq.com>
2020-11-09 08:08:45 +0800

    feat: create formList

---
+++ src/views/formShow/formList.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-04 23:42:03
+ * @LastEditTime: 2020-11-08 23:11:11
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \control-app\src\views\formShow\formList.vue
+-->
+<template>
+  <div class="form-list" formList>
+      <!-- <template v-for="(item, index) in "></template> -->
+      <div class="form-item">
+          <p></p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
